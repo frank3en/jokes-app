54ed25a396ba75b37a18bfcdc23650dd90462411
frank_shen <frank_shen@trendmicro.com>
2022-03-01 18:23:42 +0800

    feat: add styling feature

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,16 @@
 /* The <LiveReload /> component is useful during development to auto-refresh our browser whenever we make a change. Because our build server is so fast, the reload will often happen before you even notice ⚡ */
-import { LiveReload, Outlet } from "remix";
+import { LiveReload, Outlet, Links } from "remix";
+import type { LinksFunction } from "remix";
+
+import globalStylesUrl from "./styles/global.css";
+import globalStylesMediumUrl from "./styles/global-medium.css";
+import globalStylesLargeUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesUrl },
+  { rel: "stylesheet", href: globalStylesMediumUrl },
+  { rel: "stylesheet", href: globalStylesLargeUrl },
+];
 
 export default function App() {
   return (
@@ -7,6 +18,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>It's funny!</title>
+        <Links></Links>
       </head>
       <body>
         <Outlet />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,32 @@
-const HelloIndexRoute = () => {
-  return <div>Hello Index Route</div>;
+import type { LinksFunction } from "remix";
+import { Link } from "remix";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
 };
 
-export default HelloIndexRoute;
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
